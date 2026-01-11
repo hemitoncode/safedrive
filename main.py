81773765294e943eedfcf7ccc3d9f7cf33f11baf
Hemit Patel <hemitvpatel@gmail.com>
2026-01-11 10:37:35 -0500

    add video window

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import inference from InferencePipeline
-import cv2
+import cv2 
+import mediapipe as mp
+
+
+# Capture the video. The 1 parameter is webcam
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    success, image = cap.read()
+
+    if not success:
+        break
+
+    cv2.imshow("SafeDrive", cv2.flip(image, 1))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
