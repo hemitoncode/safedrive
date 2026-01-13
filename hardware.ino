0a7ef5f79d292f4e0f2f0e498b37162bb807d59b
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2026-01-13 09:56:34 -0500

    added proper keypad interaction + buzzer

--- hardware.ino
+++ hardware.ino
@@ -1,51 +1,57 @@
-#define led 2
 #include <Keypad.h>
 
-// Constants for row and column sizes
+
 const byte ROWS = 4;
-const byte COLS = 4;
- 
-// Array to represent keys on keypad
-char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+const byte COLS = 3;
+
+// Keypad setup
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
 
-// Connections to Arduino
-byte rowPins[ROWS] = {9, 8, 7, 6};
-byte colPins[COLS] = {5, 4, 3, 2};
+const int led = 2;
+const int buzzer = 12; 
 
-Keypad keyPad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// Persistent input code as Arduino String
+String inputCode = "";
+
+String validChars = "123456890";
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
-
-  digitalWrite(led, LOW); // Start by turning LED off
 }
 
 void loop() {
+  // Only read serial if available
   if (Serial.available() > 0) {
-    char message = Serial.read();
-
-    if (message == '1') {
-      digitalWrite(led, HIGH);  // LED ON
-    } 
-    else {
-      char inputKey = keyPad.getKey();
-      char inputCode;
-      int actualCode = 1234;
-
-      if (inputKey === "#" && inputCode == actualCode) {
-        Serial.write(0);
-      }
-      if (inputKey) {
-        inputCode += inputKey
-      }
-
-      digitalWrite(led, LOW);   // LED OFF
+    char data = Serial.read();
+    if (data == '1') {
+      digitalWrite(led, HIGH);
+      tone(buzzer, 1000);
     }
+  }
+
+  char key = keypad.getKey();
+
+  if (key == '#' && inputCode == "1234") {
+      digitalWrite(led, LOW);
+      noTone(buzzer);
+      inputCode = "";  // always reset after submit
+  } 
+  else if (key == '*') {
+    inputCode = "";
+  } 
+  else if (key) {
+    inputCode += key;  // append number keys only
   }
 }
